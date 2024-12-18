4f0dd73cceae0b434aa0234177152fceeb9db641
Yu-Heng Tsao <adcarry00334@gmail.com>
2024-12-18 14:08:34 -0500

    rename constant.py to constants.py

---
+++ fastgdbn/constants.py
@@ -0,0 +1,16 @@
+# The die without corresponding test result
+OUT_OF_WAFER = 0
+
+# The die that passed the applied tests
+GOOD = 1
+
+# The die that failed the applied tests
+BAD = 2
+
+# The die under investigation
+DUI = 3
+
+# Ground truth labels
+INVALID = -1
+ACCEPT = 0
+REJECT = 1
